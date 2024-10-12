5ab318c058232fe75fed1e7e5ba7dad7bb4a125e
Izabela <izabela.wlazlo@ttsw.com>
2024-10-12 21:40:31 +0200

    Links

--- src/app/views/work/work.component.ts
+++ src/app/views/work/work.component.ts
@@ -1,23 +1,45 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './work.component.html',
   styleUrl: './work.component.scss',
 })
 export class WorkComponent {
   public readonly skills: string[] = ['Angular', 'JavaScript', 'TypeScript'];
 
-  work: { [name: string]: { skills: string[] } } = {
+  work: { [name: string]: { skills: string[]; link?: string } } = {
     CV: {
-      skills: ['Angular', 'JavaScript', 'TypeScript', 'HTML/CSS'],
+      link: 'https://izabelkawl.github.io/cv/',
+      skills: [
+        'Angular',
+        'JavaScript',
+        'TypeScript',
+        'HTML/CSS',
+        'SCSS',
+        'Bootstrap',
+        'Material',
+      ],
     },
     'Busy Bee': {
-      skills: ['Angular', 'JavaScript', 'TypeScript', 'HTML/CSS'],
+      link: 'https://izabelkawl.github.io/BusyBee/',
+      skills: [
+        'Angular',
+        'JavaScript',
+        'TypeScript',
+        'HTML/CSS',
+        'SCSS',
+        'Bootstrap',
+      ],
     },
   };
+
+  goToLink(url?: string): void {
+    url && window.open(url, '_blank');
+  }
 
   get projects(): string[] {
     return Object.keys(this.work);
